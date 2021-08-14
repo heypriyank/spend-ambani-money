2d3147fdbd3a4c3b5e581173c9b91d082643f830
heypriyank <heypriyaank@gmail.com>
2021-08-14 23:59:22 +0530

    updated header

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -1,3 +1,15 @@
+import Image from "next/image";
+// import mukki from "./mukesh-ambani-rich-meme.jpg";
+
 export const Header = () => {
-  return <div className="header">Mansarovar rocks</div>;
+  return (
+    <div className="header">
+      <div className="header__text">SPEND AMBANI MONEY</div>
+      <style>
+        @import
+        url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@700&display=swap');
+      </style>
+      {/* <Image src={mukki} /> */}
+    </div>
+  );
 };
